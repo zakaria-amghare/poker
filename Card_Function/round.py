52fe910f5f9240cf3e6050c250bd60b57e04c7ab
rabah-usthb <chabanechaoucherabah4@gmail.com>
2025-10-09 17:00:01 +0100

    fixed issue in round.py where user could input choiceout of bound

--- Card_Function/round.py
+++ Card_Function/round.py
@@ -9,11 +9,20 @@ def distrebut(PlayerList:list[Player]):
         player.cardlist=gen_hand()
     
 def Player_choice(table:Table,player:Player,playerList:list[Player]):
-    choice:str = ""
+    choice:int = 0
+    minChoice:int = 1
+    maxChoice:int = 0
+    StringChoice:str = ""
     actionList :list[str] = returnPossibleAction(player,table)
     if(actionList[2] == ""):
-         choice = int(input(colored(f"1. {actionList[0]}\n2. {actionList[1]}\n","blue")))
-    choice = int(input (colored(f"1. {actionList[0]}\n2. {actionList[1]}\n3. {actionList[2]}\n","green")))
+         StringChoice = colored(f"1. {actionList[0]}\n2. {actionList[1]}\n","blue")
+         maxChoice = 2
+    else:
+         StringChoice = colored(f"1. {actionList[0]}\n2. {actionList[1]}\n3. {actionList[2]}\n","green")
+         maxChoice = 3
+    choice = int(input(StringChoice))
+    while choice<minChoice or choice>maxChoice:
+         choice = int(input(StringChoice))
     return takeAction(playerList,player,table,actionList[choice-1])
 
 
