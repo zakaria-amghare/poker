10e848dda6a9a5a58f1337e7e01034ec816e566e
zakaria-amghare <zizou.amgh@gmail.com>
2025-10-12 12:25:46 +0100

    added a basic standerd function

---
+++ theStandard.py
@@ -0,0 +1,9 @@
+from Classes.table import Table
+from Classes.player import Player
+class standatart:
+    __name__
+    dicisions_made ={}
+    def dicision():
+        pass
+    def __init__(table:Table,playerList:list[Player]):
+        pass
